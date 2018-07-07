bee4ebe5700e42ab2977600599fd6a5033b0f368
Naren Ranjit <narendran.ranjit@gmail.com>
2018-07-07 12:33:55 -0700

    working vscode debug

--- build/compile-themes.js
+++ build/compile-themes.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const jsonc = require('jsonc-parser');
+const template = require('json-templates');
 
 const colors = require('../themes/chandrian-color-theme/color-definitions.json');
 
@@ -13,29 +14,32 @@ const base = {
     tokenColors: [ ]
 };
 
+function getContents(fileName) {
+    const contents = fs.readFileSync(fileName, 'utf8');
+    const templated = template(contents)(colors);
+    const parsed = jsonc.parse(templated);
+    return parsed;
+}
+
 const basePath = path.resolve(__dirname, '../themes/chandrian-color-theme/');
 const generalStylesDir = `${basePath}/editor-colors`;
 const codeStylesDir = `${basePath}/token-colors`;
-const outputFile = path.resolve(__dirname, '../dist/chandrian-color-theme.json');
+const outputFile = path.resolve(__dirname, '../themes/chandrian-color-theme.json');
 
 const generalStyleFiles = fs.readdirSync(generalStylesDir);
 const generalColors = generalStyleFiles.reduce((accum, fileName)=> {
-    const contents = fs.readFileSync(`${generalStylesDir}/${fileName}`, 'utf8');
-    const parsed = jsonc.parse(contents);
-    Object.assign(accum, parsed);
+    const contents = getContents(`${generalStylesDir}/${fileName}`);
+    Object.assign(accum, contents);
     return accum;
 }, {});
 base.colors = generalColors;
 
 const codeStyleFiles = fs.readdirSync(codeStylesDir);
 const codeColors = codeStyleFiles.reduce((accum, fileName)=> {
-    const contents = fs.readFileSync(`${codeStylesDir}/${fileName}`, 'utf8');
-    const parsed = jsonc.parse(contents);
-    accum = accum.concat(parsed);
+    const contents = getContents(`${codeStylesDir}/${fileName}`);
+    accum = accum.concat(contents);
     return accum;
 }, []);
 base.tokenColors = codeColors;
 
-fs.writeFileSync(outputFile, JSON.stringify(base, null, 4), 'utf8');
-
-// fs.readdirSync('../themes/chandrian-color-theme');
+fs.writeFileSync(outputFile, JSON.stringify(base, null, 2), 'utf8');
