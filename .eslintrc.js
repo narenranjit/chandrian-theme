563a297a801ece4215a615da8a8b2c3bc7e0bac4
Naren Ranjit <narendran.ranjit@coursehero.com>
2021-04-28 09:53:40 -0700

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+
+  env: {
+    es6: true,
+  },
+  rules: {
+    "prettier/prettier": "warn",
+  },
+  plugins: ["prettier"],
+};
--- build/utils/compile.js
+++ build/utils/compile.js
@@ -1,64 +1,83 @@
-const fs = require('fs');
-const jsonc = require('jsonc-parser');
-const template = require('json-templates');
+const fs = require("fs");
+const jsonc = require("jsonc-parser");
+const template = require("json-templates");
 
 function parseContents(fileName, colors) {
-    const contents = fs.readFileSync(fileName, 'utf8');
-    const templated = template(contents)(colors);
-    const parsed = jsonc.parse(templated);
-    return parsed;
+  const contents = fs.readFileSync(fileName, "utf8");
+  const templated = template(contents)(colors);
+  const parsed = jsonc.parse(templated);
+  return parsed;
 }
 
 function stripItalics(colorList) {
-    return colorList.map((col)=> {
-        const dupe = Object.assign({}, col);
-        delete dupe.settings.fontStyle;
-        return dupe;
-    });
+  return colorList.map((col) => {
+    const dupe = Object.assign({}, col);
+    delete dupe.settings.fontStyle;
+    return dupe;
+  });
 }
 module.exports = function compile(paths) {
-    const colorSchemeFiles = fs.readdirSync(paths.COLOR_SCHEMES_FOLDER);
-    colorSchemeFiles.forEach((fileName)=> {
-        const contents = fs.readFileSync(`${paths.COLOR_SCHEMES_FOLDER}/${fileName}`, 'utf8');
-        const scheme = jsonc.parse(contents);
-        // console.log(scheme, contents);
-        const base = {
-            name: `Chandrian ${scheme.name}`,
-            type: scheme.type,
-            colors: {
-        
-            },
-            tokenColors: []
-        };
-        const colors = scheme.colors;
-        const outputFileName = `chandrian-${scheme.name.toLowerCase()}`;
+  const colorSchemeFiles = fs.readdirSync(paths.COLOR_SCHEMES_FOLDER);
+  colorSchemeFiles.forEach((fileName) => {
+    const contents = fs.readFileSync(
+      `${paths.COLOR_SCHEMES_FOLDER}/${fileName}`,
+      "utf8"
+    );
+    const scheme = jsonc.parse(contents);
+    // console.log(scheme, contents);
+    const base = {
+      name: `Chandrian ${scheme.name}`,
+      type: scheme.type,
+      colors: {},
+      tokenColors: [],
+    };
+    const colors = scheme.colors;
+    const outputFileName = `chandrian-${scheme.name.toLowerCase()}`;
 
-        const generalStyleFiles = fs.readdirSync(paths.GENERAL_STYLES_FOLDER);
-        const generalColors = generalStyleFiles.reduce((accum, fileName)=> {
-            const contents = parseContents(`${paths.GENERAL_STYLES_FOLDER}/${fileName}`, colors);
-            Object.assign(accum, contents);
-            return accum;
-        }, {});
-        base.colors = generalColors;
+    const generalStyleFiles = fs.readdirSync(paths.GENERAL_STYLES_FOLDER);
+    const generalColors = generalStyleFiles.reduce((accum, fileName) => {
+      const contents = parseContents(
+        `${paths.GENERAL_STYLES_FOLDER}/${fileName}`,
+        colors
+      );
+      Object.assign(accum, contents);
+      return accum;
+    }, {});
+    base.colors = generalColors;
 
-        const codeStyleFiles = fs.readdirSync(paths.CODE_STYLES_FOLDER);
-        const codeColors = codeStyleFiles.reduce((accum, fileName)=> {
-            const contents = parseContents(`${paths.CODE_STYLES_FOLDER}/${fileName}`, colors);
-            accum = accum.concat(contents);
-            return accum;
-        }, []);
-        base.tokenColors = codeColors;
+    // const semanticFiles = fs.readdirSync(paths.SEMANTICS_FOLDER);
+    // const semanticColors = semanticFiles.reduce((accum, fileName)=> {
+    //     const contents = parseContents(`${paths.SEMANTICS_FOLDER}/${fileName}`, colors);
+    //     Object.assign(accum, contents);
+    //     return accum;
+    // }, {});
+    // base.semanticHighlighting = true
+    // base.semanticTokenColors = semanticColors;
 
-        const opFile = `${paths.OP_PATH}/${outputFileName}.json`;
-        fs.writeFileSync(opFile, JSON.stringify(base, null, 2), 'utf8');
+    const codeStyleFiles = fs.readdirSync(paths.CODE_STYLES_FOLDER);
+    const codeColors = codeStyleFiles.reduce((accum, fileName) => {
+      const contents = parseContents(
+        `${paths.CODE_STYLES_FOLDER}/${fileName}`,
+        colors
+      );
+      accum = accum.concat(contents);
+      return accum;
+    }, []);
+    base.tokenColors = codeColors;
 
-        const deitalicized = Object.assign({}, base, {
-            name: `Chandrian ${scheme.name} (no italics)`,
-            tokenColors: stripItalics(base.tokenColors),
-        });
-        fs.writeFileSync(`${paths.OP_PATH}/${outputFileName}-no-italics.json`, JSON.stringify(deitalicized, null, 2), 'utf8');
+    const opFile = `${paths.OP_PATH}/${outputFileName}.json`;
+    fs.writeFileSync(opFile, JSON.stringify(base, null, 2), "utf8");
 
-        console.log('Build complete. Writing ', opFile);
-
+    const deitalicized = Object.assign({}, base, {
+      name: `Chandrian ${scheme.name} (no italics)`,
+      tokenColors: stripItalics(base.tokenColors),
     });
+    fs.writeFileSync(
+      `${paths.OP_PATH}/${outputFileName}-no-italics.json`,
+      JSON.stringify(deitalicized, null, 2),
+      "utf8"
+    );
+
+    console.log("Build complete. Writing ", opFile);
+  });
 };
