16446bacaee8d2e761c20adbf664888338ec461e
Naren Ranjit <narendran.ranjit@coursehero.com>
2021-04-27 21:34:43 -0700

    add additional annotation colors

--- demo/tsx.tsx
+++ demo/tsx.tsx
@@ -1,78 +1,27 @@
-import { useState, useEffect } from 'react';
-import useInterval from './useInterval';
-
-// return Type of useCOunter
-type ReturnValueUseCounter = [
-  number, // value
-  {
-    inc(step?: number): void;
-    dec(step?: number): void;
-    get(): number;
-    set(count: number): void;
-    reset(count?: number): void;
-  }
-];
-
-interface ICounter {
-  initialCount?: number;
-  onReset?: () => void;
-  onAfterReset?: (initialCount: number) => void;
-  options?: {
-    autoIncrementCounter: boolean;
-  };
+import React, { useState } from "react"
+interface Props {
+  children: (x: number, y: number) => JSX.Element | null
 }
-
-let renderCounter = 0;
-
-const defaultOptions = {
-  autoIncrementCounter: true,
-};
-
-const useCounter = ({
-  initialCount = 0,
-  onReset,
-  onAfterReset,
-  options = defaultOptions,
-}: ICounter): ReturnValueUseCounter => {
-  const [count, setCount] = useState(initialCount);
+const Mouse: React.FC<Props> = ({ children }) => {
   const [state, setState] = useState({
-    resetTriggered: false,
-  });
-  // Event handlers
-  const inc = (step: number = 1) => {
-    setCount(count + step);
-  };
-  const dec = (step: number = 1) => {
-    setCount(count - step);
-  };
-
-  const get = () => count;
-
-  const reset = (count: number = initialCount) => {
-    setCount(count);
-    setState({ ...state, resetTriggered: true });
-
-    if (onReset) {
-      onReset();
-    }
-  };
-
-  // Side effects
-  useEffect(() => {
-    if (state.resetTriggered && onAfterReset) {
-      setState({ ...state, resetTriggered: false });
-      onAfterReset(initialCount);
-    }
-  }, [count]);
-
-  options.autoIncrementCounter &&
-    useInterval(() => setCount(count => count + 1), 1000);
-
-  renderCounter++;
-
-  console.info('render counter', renderCounter);
-
-  return [count, { inc, dec, get, set: setCount, reset }];
-};
-
-export default useCounter;
+    x: 0,
+    y: 0,
+  })
+  const handleMouseMove = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    setState({
+      x: event.clientX,
+      y: event.clientY,
+    })
+  }
+  return (
+    <div
+      style={{ height: "100%", position: "relative" }}
+      onMouseMove={handleMouseMove}
+    >
+      {children(state.x, state.y)}
+    </div>
+  )
+}
+export default Mouse
